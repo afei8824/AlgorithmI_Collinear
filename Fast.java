7a9a249ce10a6981af1ada9fdc62c46ff40c1809
Pengfei Zheng <afei8824@gmail.com>
2014-03-03 17:44:32 -0800

    update
    finish fast.java

--- Fast.java
+++ Fast.java
@@ -7,6 +7,8 @@ public class Fast{
         Point[] points = null;
         Point[] pointsInvariant = null;
         int index = 0;
+        HashMap<String, ArrayList<Point>> linesFound = new HashMap<String, ArrayList<Point>>();
+
         try{
             FileInputStream fis = new FileInputStream(args[0]);
             BufferedReader br = new BufferedReader(new InputStreamReader(fis));
@@ -33,6 +35,7 @@ public class Fast{
         int segment = 1;
         for (int i = 0; i < pointsInvariant.length; i++ ){
             Point p = pointsInvariant[i];
+            p.draw();
             Arrays.sort(points,p.SLOPE_ORDER);
             for (int j = 0; j < points.length - 1; j = j + segment){
                 segment = 1;
@@ -40,13 +43,36 @@ public class Fast{
                 for(int k = j+1; k < points.length - 1; k++){
                     if (points[k].slopeTo(p) != slope){
                         break;
-                    }
+                    }                   
                     segment ++;
                 }
-                if(segment >= 4){
-                    StdOut.println("GOT");
+                //System.out.println(points[j]+" to "+p+" is "+slope+" and segment = "+segment);
+                if(segment >= 3){
+                    ArrayList<Point> tempLine = new ArrayList<Point>();
+                    tempLine.add(p);
+                    for(int l = j; l < j+ segment; l++){
+                        tempLine.add(points[l]);
+                    }
+                    Collections.sort(tempLine);
+                    StringBuilder sb = new StringBuilder();
+                    for(int l = 0; l < tempLine.size();l++){
+                        sb.append(tempLine.get(l));
+                        if( l != tempLine.size() -1 )
+                            sb.append(" -> "); 
+                    }
+                    //StdOut.println(sb);
+                    if(!linesFound.containsKey(sb.toString())){
+                        linesFound.put(sb.toString(), tempLine);
+                    }
                 }
-            }        
+            }
+            //System.out.println("========================="+p);
+        }
+
+        for(String myLine: linesFound.keySet()){
+            ArrayList<Point> myLinePoints = linesFound.get(myLine);
+            myLinePoints.get(0).drawTo(myLinePoints.get(myLinePoints.size()-1));
+            StdOut.println(myLine);
         }
 
     }
